e4858de064beba5985ea01a5551ed75489b28f99
rosamondkim <itzmems@naver.com>
2023-01-22 23:46:37 +0900

    day4

---
+++ day4.js
@@ -0,0 +1,8 @@
+// type alias(별칭) 만들고 변수에 담기
+var 동물 = 123;
+{
+    name: "kim", age;
+    20;
+}
+;
+var 동물2 = { name: 'kim', age: 20 };
---
+++ day4.ts
@@ -0,0 +1,8 @@
+// type alias(별칭) 만들고 변수에 담기
+
+type Animal = string | number | undefined;
+let 동물: Animal = 123;
+
+type Animal2 = { name: string; age: number } = { name: "kim", age: 20 };
+let 동물2:Animal2 ={name:'kim',age:20}
+
