5898cff294b43aa894d12a3450eead8bea070748
rosamondkim <itzmems@naver.com>
2023-01-28 11:00:00 +0000

    day5

--- day5.ts
+++ day5.ts
@@ -26,6 +26,8 @@ function 가위바위보(a: "가위" | "바위" | "보"): ("가위" | "바위" |
 
 // const 변수의 한계
 const 변수 = "대머리";
+
+// Literal Type => string,number만 타입이 될 수 있는게 아니라, 일반 글자도 타입 될 수 있음.
 let 리터럴이짱이다: "대머리" | "머머리";
 // literal type은 const 변수와 유사하게 사용 가능. but 여러개 지정 가능
 
